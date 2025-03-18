a7bbcb730a4f4f373a367bc24f69149bc8022dd1
tharindu-yb <tharinduyapabandara@gmail.com>
2025-03-18 11:42:34 +0530

    updated firebase_options_test.dart file

--- bloom2/bloomiot/test/firebase_options_test.dart
+++ bloom2/bloomiot/test/firebase_options_test.dart
@@ -1,16 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/foundation.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:bloomiot/firebase_options.dart';
 
 void main() {
   group('DefaultFirebaseOptions', () {
+    test('throws UnsupportedError for web platform', () {
+      // Simulate web platform by using kIsWeb
+      if (kIsWeb) {
+        expect(() => DefaultFirebaseOptions.currentPlatform,
+            throwsA(isA<UnsupportedError>()));
+      }
+    });
+
     test('returns correct FirebaseOptions for Android', () {
-      // Simulating Android platform
+      // Simulate Android platform
       debugDefaultTargetPlatformOverride = TargetPlatform.android;
 
       final options = DefaultFirebaseOptions.currentPlatform;
 
+      // Check if the returned FirebaseOptions match the expected Android values
       expect(options.apiKey, 'AIzaSyCmphU4NwDnAjcyCLPglIM3ybNxXwjoZNY');
       expect(options.appId, '1:957472013508:android:7380486c849cc4e3015d91');
       expect(options.messagingSenderId, '957472013508');
@@ -21,11 +29,12 @@ void main() {
     });
 
     test('returns correct FirebaseOptions for iOS', () {
-      // Simulating iOS platform
+      // Simulate iOS platform
       debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
 
       final options = DefaultFirebaseOptions.currentPlatform;
 
+      // Check if the returned FirebaseOptions match the expected iOS values
       expect(options.apiKey, 'AIzaSyDidR2gmTY2mSLgejhvmrJHMMBr_9vuXZk');
       expect(options.appId, '1:957472013508:ios:0b70a5e5a111d617015d91');
       expect(options.messagingSenderId, '957472013508');
@@ -36,26 +45,23 @@ void main() {
       expect(options.iosBundleId, 'com.example.bloomiot');
     });
 
-    test('throws UnsupportedError for unsupported platforms', () {
-      // Simulating unsupported platforms
+    test(
+        'throws UnsupportedError for unsupported platforms (macOS, windows, linux)',
+        () {
+      // Test for macOS
       debugDefaultTargetPlatformOverride = TargetPlatform.macOS;
       expect(() => DefaultFirebaseOptions.currentPlatform,
           throwsA(isA<UnsupportedError>()));
 
+      // Test for Windows
       debugDefaultTargetPlatformOverride = TargetPlatform.windows;
       expect(() => DefaultFirebaseOptions.currentPlatform,
           throwsA(isA<UnsupportedError>()));
 
+      // Test for Linux
       debugDefaultTargetPlatformOverride = TargetPlatform.linux;
       expect(() => DefaultFirebaseOptions.currentPlatform,
           throwsA(isA<UnsupportedError>()));
-    });
-
-    test('throws UnsupportedError for web platform', () {
-      // Simulating Web platform
-      expect(() => DefaultFirebaseOptions.currentPlatform,
-          throwsA(isA<UnsupportedError>()),
-          skip: !kIsWeb);
     });
   });
 }
