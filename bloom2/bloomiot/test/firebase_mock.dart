9e2a3c1cc9ba6076eccfe528a999e4c5705b2aea
tharindu-yb <tharinduyapabandara@gmail.com>
2025-03-18 09:44:37 +0530

    changes firebase_mock.dart

--- bloom2/bloomiot/test/firebase_mock.dart
+++ bloom2/bloomiot/test/firebase_mock.dart
@@ -1,9 +1,1 @@
-import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_test/flutter_test.dart';
 
-// Mock Firebase
-Future<void> setupFirebaseMocks() async {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-}
