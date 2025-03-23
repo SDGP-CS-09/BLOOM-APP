fdb182a6a43313d492670f359d8b6d57362fa493
Tharindu <tharinduyapabandara@gmail.com>
2025-03-23 22:40:17 +0530

    Add files via upload

---
+++ supabase_mock.dart
@@ -0,0 +1,8 @@
+import 'package:mockito/annotations.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+@GenerateMocks([
+  SupabaseClient,
+  PostgrestFilterBuilder,
+])
+void main() {}
